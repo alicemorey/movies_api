9f6b56d971eab13218641ca902c9ad22e5b13fd7
alicemorey <alicemmorey@gmail.com>
2024-04-30 15:44:22 +0200

    edits models.js

--- models.js
+++ models.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt=require ('bcrypt');
 
 // Define movieSchema format to be used in MongoDB
-let movies1Schema = mongoose.Schema({
+let movieSchema = mongoose.Schema({
     Title: { type: String, required: true },
     Description: { type: String, required: true },
     Genre: { 
@@ -19,26 +19,27 @@ let movies1Schema = mongoose.Schema({
 });
 
 // Define schema for User
-let users1Schema = mongoose.Schema({
+let userSchema = mongoose.Schema({
     Username: { type: String, required: true },
     Email: { type: String, required: true },
     Password: { type: String, required: true },
     Birthday: {type:Date, required: true},
-    FavoriteMovie: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
 //convert user password into hashed format using bcrypt.js
-users1Schema.statics.hashPassword=(password)=>{
+userSchema.statics.hashPassword=(password)=>{
     return bcrypt.hashSync(password, 10);
 };
 
 //validates user password by compating it with the stored hashed password
-users1Schema.methods.validatePassword=function(password){
+userSchema.methods.validatePassword=function(password){
     return bcrypt.compareSync(password, this.Password);
 };
 
 // create models on defined schemas
-const users1 = mongoose.model('User', users1Schema);
-const movies1 = mongoose.model('Movie', movies1Schema);
+let User = mongoose.model("User", userSchema);
+const Movie = mongoose.model("Movie", movieSchema);
 
-module.exports = { users1, movies1 };
+//exports defined modules so they can be used
+module.exports = { User, Movie };
