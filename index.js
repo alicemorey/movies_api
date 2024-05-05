973f5e0dc6e2d23f2585a9a99ac05f8dcf3b27cb
alicemorey <alicemmorey@gmail.com>
2024-05-05 17:49:02 +0200

    online

--- index.js
+++ index.js
@@ -68,10 +68,10 @@ mongoose.connect("mongodb://localhost:27017/myflix", {
 /**
  * connection to online database hosted by mongoDB
  */
-// mongoose.connect(process.env.CONNECTION_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+mongoose.connect( process.env.CONNECTION_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+ });
 
 app.use(express.static("public"));
 
