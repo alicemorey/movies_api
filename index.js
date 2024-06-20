a0e686c1f1b93e5b22d803157713b71793265e1a
alicemorey <alicemmorey@gmail.com>
2024-06-20 21:30:18 +0200

    Revert "adding CORS"
    This reverts commit a02f5649c02af9020c200fc8102a3acfeacc6f87.

--- index.js
+++ index.js
@@ -502,21 +502,6 @@ app.use((err, req, res, next) => {
   res.status(500).send("It's not working right now!");
 });
 
-const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 
-'https://myflix2024.netlify.app/'];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
-
 // starts server on specified port and listens for requests
 const port = process.env.PORT || 8080;
 app.listen(port, "0.0.0.0", () => {
